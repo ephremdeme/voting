62947b6baf0389711314e371821ad957b2e0f43b
Ephrem Demelash <demelashephrem@gmail.com>
2020-10-13 00:00:08 +0300

    👌 IMPROVE:  Footer UI

--- web_client/src/components/Footer.js
+++ web_client/src/components/Footer.js
@@ -1,29 +1,52 @@
-import React from 'react';
-import { MDBFooter,MDBRotatingCard, MDBBtn, MDBIcon } from 'mdbreact';
+import React from "react";
+import { MDBFooter, MDBContainer, MDBCol, MDBRow, MDBIcon } from "mdbreact";
+
 
 const Footer = () => {
-    return (
-        <MDBFooter color="blue" className="text-center font-small darken-2">
-            <div className="pt-4">
-                <MDBBtn outline color="white" tag="a" href="https://mdbootstrap.com/docs/react/getting-started/download/" target="_blank">Download MDB<MDBIcon icon="download" className="ml-2"/></MDBBtn>
-                <MDBBtn outline color="white" tag="a" href="https://mdbootstrap.com/docs/react/getting-started/quick-start/" target="_blank">Start free tutorial<MDBIcon icon="graduation-cap" className="ml-2"/></MDBBtn>
-                <hr className="my4"/>
+  return (
+    <MDBFooter color="cyan" className="font-small darken-3 pt-0">
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol md="12" className="py-5">
+            <div className="mb-5 flex-center">
+              <a className="fb-ic">
+                <i className="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+              </a>
+              <a className="tw-ic">
+                <i className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+              </a>
+              <a className="gplus-ic">
+                <i className="fab fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+              </a>
+              <a className="li-ic">
+                <i className="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+              </a>
+              <a className="ins-ic">
+                <i className="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+              </a>
+              <a className="pin-ic">
+                <i className="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+              </a>
             </div>
-            <div className="pb-4">
-                <MDBIcon fab icon="facebook" className="mr-3"/>
-                <MDBIcon fab icon="twitter" className="mr-3"/>
-                <MDBIcon fab icon="youtube" className="mr-3"/>
-                <MDBIcon fab icon="google-plus" className="mr-3"/>
-                <MDBIcon fab icon="dribbble" className="mr-3"/>
-                <MDBIcon fab icon="pinterest" className="mr-3"/>
-                <MDBIcon fab icon="github" className="mr-3"/>
-                <MDBIcon fab icon="codepen" className="mr-3"/>
-            </div>
-            <p className="footer-copyright mb-0 py-3 text-center">
-                &copy; {new Date().getFullYear()} Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
-            </p>
-        </MDBFooter>
-    );
-}
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright:{" "}
+          <a
+            className="border border-light rounded mr-1 nav-link Ripple-parent"
+            rel="noopener noreferrer"
+            href="https://github.com/ephremdeme/voting"
+            target="_blank"
+          >
+            <MDBIcon fab icon="github" className="mr-2" />
+            Github
+          </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+  );
+};
 
 export default Footer;
